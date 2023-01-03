f80d78e550dce0bdb24bfc57f3b8bb55d7aae277
Franklyn Silva <eng.franklyn@icloud.com>
2023-01-03 12:05:18 -0300

    adição de novos componentes

--- src/components/Feijao.vue
+++ src/components/Feijao.vue
@@ -1,27 +1,24 @@
 <template>
-    <footer id="footer">
-        <p>Make Your Burguer &copy;2022</p>
-    
-    </footer>
-    
+  <footer id="footer">
+    <p>Make Your Burguer &copy;2022</p>
+  </footer>
 </template>
 
 <script>
 export default {
-    name: 'Feijao'
-}
+  name: "Feijao",
+};
 </script>
 
 <style scoped>
 #footer {
-    height: 200px;
-    background-color: #222;
-    border-top: 4px solid #111;
-    color: #FCBA03;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    
+  height: 200px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 }
 </style>
---
+++ src/components/Pedra.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="input-container">
+    <label for="value">{{ text }}</label>
+    <select name="value" id="value" v-model="value">
+      <option v-for="value in data" :key="value.id" :value="value.tipo">
+        {{ value.tipo }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pedra",
+  props: {
+    data: Object,
+    text: String,
+  },
+};
+</script>
+
+<style scoped>
+.input-container {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 15px;
+  color: #222;
+  padding: 5px 10px;
+  border-left: 4px solid #fcba03;
+}
+
+input,
+select {
+  padding: 5px 10px;
+  width: 300px;
+}
+</style>
